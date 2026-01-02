1c3852d04d09db2963c43fd1bb58241a422fd6a4
adityash23 <142367043+adityash23@users.noreply.github.com>
2026-01-02 20:23:07 +0000

    Deploying to gh-pages from @ adityash23/adityash23.github.io@988ef9457a3361e21d1caafb1e9a922f3a79baf1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/books/";
           },
-        },{id: "post-bloom-filters",
+        },{id: "post-finite-difference-vs-backpropagation",
+        
+          title: "Finite Difference vs Backpropagation",
+        
+        description: "comparison between two gradient computation techniques in machine learning",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/forward-vs-back-prop/";
+          
+        },
+      },{id: "post-bloom-filters",
         
           title: "Bloom Filters",
         
@@ -52,7 +63,7 @@ ninja.data = [{
         
           title: "Code to Give 2025",
         
-        description: "attending hackathon by Morgan Stanley",
+        description: "attending the hackathon organized by Morgan Stanley",
         section: "Posts",
         handler: () => {
           
@@ -171,8 +182,8 @@ ninja.data = [{
             },},{id: "news-my-project-translateasl-won-first-place-in-the-maishacks-2025-try-it-out",
           title: 'My project translateASL won First Place in the MAISHacks 2025. Try it out!...',
           description: "",
-          section: "News",},{id: "news-i-m-awarded-a-sura-for-summer-2025",
-          title: 'I’m awarded a SURA for Summer 2025',
+          section: "News",},{id: "news-i-m-awarded-a-sura-merit-based-research-award-for-summer-2025-and-will-be-working-in-disl",
+          title: 'I’m awarded a SURA, merit-based research award, for Summer 2025 and will be...',
           description: "",
           section: "News",},{id: "news-i-attented-canada-s-biggest-hackathon-hackthenorth-2025-at-the-university-of-waterloo-check-out-our-project-cogniverse-making-every-idea-teachable",
           title: 'I attented Canada’s biggest hackathon - HackTheNorth 2025 at the University of Waterloo....',
@@ -191,47 +202,21 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/aditya-sharma-b9b35228a", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/adityash23", "_blank");
+        },
+      },{
         id: 'social-discord',
         title: 'Discord',
         section: 'Socials',
@@ -244,20 +229,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("mailto:%61%64%69%74%79%61.%73%68%61%72%6D%61%33@%6D%61%69%6C.%6D%63%67%69%6C%6C.%63%61", "_blank");
-        },
-      },{
-        id: 'social-github',
-        title: 'GitHub',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://github.com/adityash23", "_blank");
-        },
-      },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/aditya-sharma-b9b35228a", "_blank");
         },
       },{
         id: 'social-rss',
